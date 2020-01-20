b2e1d5af2503d50eb00234cf116ec91a59823a19
Anillab <anillabusolo@gmail.com>
2020-01-20 15:17:36 +0000

    define the data graph

---
+++ start/client/src/schema.js
@@ -0,0 +1,28 @@
+const {gql} =require('apollo-server');
+
+const typeDefs=gql`
+type Launch{
+  id:ID!
+  site:String
+  mission:Mission
+  rocket:Rocket
+  isBooked:Boolean!
+}
+type Rocket{
+  id:ID!
+  email:String!
+  trips:[Launch]!
+
+}
+type Mission{
+  name:String
+  missionPatch(size:PatchSize):String
+}
+enum PatchSize{
+  SMALL
+  LARGE
+}
+
+`;
+
+module.exports=typeDefs;
