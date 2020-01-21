647990c388a4b4fd847cc715d43b7c98ed434059
Anillab <anillabusolo@gmail.com>
2020-01-21 10:09:15 +0000

    moved content to schema.js in the server side

--- start/client/src/schema.js
+++ start/client/src/schema.js
@@ -1,6 +1,8 @@
 const {gql} =require('apollo-server');
 
 const typeDefs=gql`
+
+
 type Launch{
   id:ID!
   site:String
@@ -10,9 +12,14 @@ type Launch{
 }
 type Rocket{
   id:ID!
+  name:String
+  type:String
+
+}
+type User{
+  id:ID!
   email:String!
   trips:[Launch]!
-
 }
 type Mission{
   name:String
@@ -22,7 +29,21 @@ enum PatchSize{
   SMALL
   LARGE
 }
-
+type Query{
+  launches:[Launch]!
+  launch(id:ID!):Launch
+  me:User
+}
+type Mutation{
+  bookTrips(launchIds:[ID]!):TripUpdateResponse!
+  cancelTrip(launchId:ID!):TripUpdateResponse!
+  login(email:String):String
+}
+type TripUpdateResponse{
+  success:Boolean!
+  message:String
+  launches:[Launch]
+}
 `;
 
 module.exports=typeDefs;
